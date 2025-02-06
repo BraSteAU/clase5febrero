378fd0cd60aaf1bb9e1986f7914a3bdba3620694
BraSteAU <brayan_8924@hotmail.com>
2025-02-05 19:51:08 -0500

    clase 5 de febrero

---
+++ clase_5_febrero.py
@@ -0,0 +1,7 @@
+#Sesion 6 y 7: tipos de funciones, parametros y referencias.
+
+#Objetivos:
+#   1)Comprender los diferentes tipos de funciones y sus parametros.
+#   2)Aplicar funciones con parametros y valores de retorno en problemas complejos.
+#   3)Implementar busquedas y ordenaciones detro de listas y matrices.
+#   4)Resolver problemas con proramacion modular.
